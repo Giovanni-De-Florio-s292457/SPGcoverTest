98d6097f616edb4d775754296d54f1f2b33d024e
Giovanni De Florio s292457@studenti.polito.it <giovannidf96@gmail.com>
2021-11-30 09:41:27 +0100

    c3

--- client/cypress/integration/story2.spec.js
+++ client/cypress/integration/story2.spec.js
@@ -28,7 +28,7 @@ describe(specTitle('New customer registration'), () => {
     })
 })
 
-describe('New customer registration made by the shopper Employee', () => {
+describe(specTitle('New customer registration made by the shopper Employee'), () => {
     it('successfully loads', () => {
         cy.visit('http://localhost:3000') // change URL to match your dev URL
         cy.contains('Login').click()
